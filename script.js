8080a585c62823aa3846b7016ce2e15e6cd07b15
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-02-19 19:56:57 -0500

    updates

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let fetched = fetch('https://mod-server.lamaqdahodwala.repl.co/everyone')
-  .then(response => response.text());
+function grab(){
+	function createtable(data){
+		let users = data;
+		let temp = users.split('')
+		let str = ''
+		for (var i of temp){
+			var added = ''
+			if (i == "'"){
+				added = '"'
+			} else {
+				added = i
+			}
+			str += added
+		}
+		console.log(str)
+		var map = new Map(Object.entries(JSON.parse(str)))
+		console.log(map)
+		for (var i of map.entries()){
+			console.log(i)
+		}
 
-let waitforstatus = async() => {
-    let x = await fetched
-    console.log(x)
+		
+	}
+	let fetched = fetch('https://mod-server.lamaqdahodwala.repl.co/everyone')
+		.then(response => response.text())
+		.then(alert('Hold on, loading!'))
+		.then(data => createtable(data))
 }
